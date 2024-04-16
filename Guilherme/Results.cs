84786cdf50bfae4778fe2f5439856f80e2c5917b
Guilherme <gui87981@gmail.com>
2024-04-16 11:52:38 -0300

    p2 16/04/2024

---
+++ Guilherme/Results.cs
@@ -0,0 +1,28 @@
+using CoreImage;
+using MapKit;
+
+namespace Guilherme;
+  public class Result
+{
+    public int temp {get; set;}
+    public string date {get; set;}
+    public int time {get; set;}
+    public string description {get; set;}
+    public int cloudiness {get; set;}
+    public string currently {get; set;}
+    public int img_id {get; set;}
+    public string city {get; set;}
+    public int humidity {get; set;}
+    public string rain {get; set;}
+    public int city_name {get; set;}
+    public string sunrise {get; set;}
+    public int windy_speedy {get; set;}
+    public string moon_phase {get; set;}
+    public int windy_direction {get; set;}
+    public string windy_slug {get; set;}
+    public int sunset {get; set;}
+    public string condition_slug {get; set;}
+    public int timezone {get; set;}
+    public string condition_code {get; set;}
+
+} 
